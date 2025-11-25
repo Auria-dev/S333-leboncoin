e996e34cc85df767f99408133076e53ade0bab5a
auria-dev <julien.berst@etu.univ-smb.fr>
2025-11-25 21:07:06 +0000

    css & html structure

--- resources/views/form-recherche.blade.php
+++ resources/views/form-recherche.blade.php
@@ -3,10 +3,29 @@
 @section('title', 'Rechercher')
 
 @section('content')
-  <form method="post" action="{{ url('resultats') }}">
-    @csrf
-    <input name="search"/>
-    <input type="submit"/>
-  </form>
-@endsection
+<div class="container">
+    
+    <main>
+        <form method="post" action="{{ url('resultats') }}">
+            @csrf
 
+            <div class="form-group">
+                <label for="search">Nom de ville</label>
+                <input 
+                    type="text" 
+                    id="search" 
+                    name="search" 
+                    placeholder="Ex: Paris, Lyon..." 
+                    required
+                />
+            </div>
+
+            <div class="form-group">
+                <input type="submit" value="Rechercher"/>
+            </div>
+
+        </form>
+    </main>
+
+</div>
+@endsection
