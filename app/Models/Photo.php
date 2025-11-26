35917895ec897fb956ba7467a61849bccdedd6a6
VYyo <yoenie.dupuy@etu.univ-smb.fr>
2025-11-26 08:20:48 +0000

    photos dans liste res recherche

---
+++ app/Models/Photo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+    use HasFactory;
+
+    protected $table = "photo";
+    protected $primaryKey = "idphoto";
+    public $timestamps = false;
+
+    
+    public function annonce() {
+        return $this->belongsTo(Annonce::class, "idannonce");
+    }
+}
