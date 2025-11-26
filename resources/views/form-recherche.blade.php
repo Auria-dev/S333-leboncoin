e61b70cfeb9e9ca668cb406f2887b034efba5c1f
auria-dev <julien.berst@etu.univ-smb.fr>
2025-11-26 19:16:44 +0000

    completely new face! (new css)

--- resources/views/form-recherche.blade.php
+++ resources/views/form-recherche.blade.php
@@ -3,30 +3,22 @@
 @section('title', 'Rechercher')
 
 @section('content')
-<div class="container">
-    
-    <main>
-        <form method="post" action="{{ url('resultats') }}">
-            @csrf
+    <form method="post" action="{{ url('resultats') }}" style="width: 100%; max-width: 400px; margin: 0 auto;">
+        @csrf
 
-            <div class="form-group">
-                <label for="search">Nom de ville</label>
-                <input 
-                    type="text" 
-                    id="search" 
-                    name="search" 
-                    placeholder="Ex: Paris, Lyon..." 
-                    style="width:100%;"
-                    required
-                />
-            </div>
+        <div class="full-width">
+            <label for="search">Nom de ville</label>
+            <input 
+                type="text" 
+                id="search" 
+                name="search" 
+                placeholder="Ex: Paris, Lyon..." 
+                required
+            />
+        </div>
 
-            <div class="form-group">
-                <input type="submit" value="Rechercher" style="width:100%;"/>
-            </div>
-
-        </form>
-    </main>
-
-</div>
+        <div class="full-width">
+            <input type="submit" class="submit-btn" value="Rechercher" />
+        </div>
+    </form>
 @endsection
