2c95b059babe5a58774b99fa5233a9d39a5b172d
auria-dev <julien.berst@etu.univ-smb.fr>
2025-11-27 08:57:25 +0000

    filtrage par date

--- app/Models/Calendrier.php
+++ app/Models/Calendrier.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,6 +13,7 @@ class Calendrier extends Model
     protected $table = "calendrier";
     protected $primaryKey = ["iddate", "idannonce"];
     public $timestamps = false;
+    public $incrementing = false;
 
     public function date() {
         return $this->belongsTo(Date::class, "iddate");
@@ -18,5 +21,51 @@ class Calendrier extends Model
 
     public function annonce() {
         return $this->belongsTo(Annonce::class, "idannonce");
+    }
+
+
+    //
+    //
+    // https://stackoverflow.com/questions/36332005/laravel-model-with-two-primary-keys-update
+    //
+    //
+    
+    /**
+     * Set the keys for a save update query.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    protected function setKeysForSaveQuery($query)
+    {
+        $keys = $this->getKeyName();
+        if(!is_array($keys)){
+            return parent::setKeysForSaveQuery($query);
+        }
+
+        foreach($keys as $keyName){
+            $query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+        }
+
+        return $query;
+    }
+
+    /**
+     * Get the primary key value for a save query.
+     *
+     * @param mixed $keyName
+     * @return mixed
+     */
+    protected function getKeyForSaveQuery($keyName = null)
+    {
+        if(is_null($keyName)){
+            $keyName = $this->getKeyName();
+        }
+
+        if (isset($this->original[$keyName])) {
+            return $this->original[$keyName];
+        }
+
+        return $this->getAttribute($keyName);
     }
 }
