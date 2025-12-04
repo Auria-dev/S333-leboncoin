6f3d91449b9f9ea5537459989f549d0c92350502
auria-dev <julien.berst@etu.univ-smb.fr>
2025-12-04 09:29:52 +0000

    calendar & price

--- app/Http/Controllers/AnnonceController.php
+++ app/Http/Controllers/AnnonceController.php
@@ -8,23 +8,43 @@ use App\Models\Ville;
 use App\Models\Annonce;
 use App\Models\TypeHebergement;
 use App\Models\Favoris;
+use App\Models\Calendrier;
 
-class AnnonceController extends Controller
-{
+class AnnonceController extends Controller {
   public function view($id) {
     $iduser = -1;
 
     if (Auth::check()) {
-      $iduser = auth()->user()->idutilisateur;
+        $iduser = auth()->user()->idutilisateur;
     }
 
     $exists = Favoris::where('idutilisateur', '=', $iduser)
             ->where('idannonce', '=', $id)
             ->exists();
 
+    $disponibilites = Calendrier::where('idannonce', $id)
+        ->whereHas('date', function($q) {
+            $q->where('date', '>=', now()->toDateString());
+        })
+        ->with('date')
+        ->get();
+
+    $dispoMap = [];
+    
+    foreach($disponibilites as $cal) {
+        if ($cal->date) {
+            $dateString = \Carbon\Carbon::parse($cal->date->date)->format('Y-m-d');
+            
+            $dispoMap[$dateString] = [
+                'dispo' => (bool)$cal->code_dispo, 
+            ];
+        }
+    }
+    
     return view ("detail-annonce", [
-      'annonce'=>Annonce::findOrFail($id), 
-      'isFav' => $exists 
+        'annonce'   => Annonce::findOrFail($id), 
+        'isFav'     => $exists,
+        'dispoJson' => json_encode($dispoMap) 
     ]);
   }
 
