d4d09addc0275861b4cff4b52d2bf26470595ca2
VYyo <yoenie.dupuy@etu.univ-smb.fr>
2025-12-06 11:26:07 +0000

    Ajouter annonce, en attente de css

--- app/Http/Controllers/AnnonceController.php
+++ app/Http/Controllers/AnnonceController.php
@@ -11,6 +11,11 @@ use App\Models\TypeHebergement;
 use App\Models\Favoris;
 use App\Models\Calendrier;
 use App\Models\Particulier;
+use App\Models\Photo;
+use App\Models\Equipement;
+use App\Models\Equipe;
+use App\Models\Service;
+use App\Models\Propose;
 
 class AnnonceController extends Controller {
   public function view($id) {
@@ -77,7 +82,9 @@ class AnnonceController extends Controller {
 
   function afficher_form() {
     $types = TypeHebergement::all();
-    return view("ajouter-annonce",  ['types' => $types]);
+    $equipements = Equipement::all();
+    $services = Service::all();
+    return view("ajouter-annonce",  ['types' => $types, 'equipements' => $equipements, 'services' => $services]);
   }
 
   function ajouter_annonce(Request $req) {
@@ -86,6 +93,7 @@ class AnnonceController extends Controller {
       $user = auth()->user();
       $iduser = auth()->user()->idutilisateur;
     }
+
     $typeCompte = $user->getTypeParticulier(); 
     if($typeCompte == 'Locataire') {
       Particulier::where('idparticulier', $iduser)->update(['code_particulier' => 2]);
@@ -110,6 +118,7 @@ class AnnonceController extends Controller {
 
     $codeville = Ville::where('nom_ville', $req->ville)->first();
     $type_heb = TypeHebergement::where('nom_type_hebergement', $req->DepotTypeHebergement)->first();
+    $service = Service::where('nom_service', $req->DepotService)->first();
   
     // this says
     $annonce = Annonce::create([
@@ -132,6 +141,48 @@ class AnnonceController extends Controller {
         'latitude' => null,
     ]);
 
-    return redirect(RouteServiceProvider::HOME);
+    $num_photo = 1;
+    if($req->hasFile('file')) {
+      foreach($req->file('file') as $file)	{    
+        $filePath = $file->getClientOriginalName();
+        $fileName = '/images/photo_annonce_' . $user->idutilisateur . '_' . $filePath . '.jpg';
+        // $dbFileName = "/images/" . $fileName;
+        $file->move(public_path('images'), $fileName);
+        $url = asset('images/'. $fileName);
+
+        $photo = Photo::create([
+          'idannonce' => $annonce->idannonce,
+          'nomphoto' => $fileName,
+          'legende' => null,
+        ]);
+
+        $num_photo++;
+      }
+    }
+    else {
+      $photo = Photo::create([
+          'idannonce' => $annonce->idannonce,
+          'nomphoto' => "/images/photo-annonce.jpg",
+          'legende' => null,
+        ]);
+    }
+
+    $nomsEquipements = $req->DepotEquipement;
+    if (!empty($nomsEquipements) && is_array($nomsEquipements)) {
+      $idsEquipements = Equipement::whereIn('nom_equipement', $nomsEquipements)
+        ->pluck('idequipement')
+        ->toArray();
+      $annonce->equipement()->sync($idsEquipements);
+    }
+
+    $nomsServices = $req->DepotService;
+    if (!empty($nomsServices) && is_array($nomsServices)) {
+      $idsServices = Service::whereIn('nom_service', $nomsServices)
+        ->pluck('idservice')
+        ->toArray();
+      $annonce->service()->sync($idsServices);
+    }
+
+    return redirect('/profile');
   }
 }
