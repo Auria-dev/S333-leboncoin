b82f3460db534a6ecb7d9fdc81f8293f3d6334e4
auria-dev <julien.berst@etu.univ-smb.fr>
2025-12-06 18:57:27 +0000

    fixed a bunch of small things

--- resources/views/detail-proprietaire.blade.php
+++ resources/views/detail-proprietaire.blade.php
@@ -32,48 +32,46 @@
 
     <div style="margin-bottom: 1rem; margin-left: 1rem; color: #4A3B32;"><strong>Annonce(s) publiées par {{$proprietaire->nom_utilisateur . ' ' . $proprietaire->prenom_utilisateur}}</strong></div>
 
-    <div class="scroll">
-        @foreach($proprietaire->annonce as $annonce)
-            <a class="annonce-card" href="{{ url('annonce/'.strval($annonce->idannonce)) }}">
-                
-                <div class="annonce-photo">
-                    @if(isset($annonce->photo) && count($annonce->photo) > 0)
-                        <img loading="lazy" src="{{ $annonce->photo[0]->nomphoto }}" alt="Photo annonce"/>
+        <div class="res-scroller">
+            @forelse($proprietaire->annonce as $similaire)
+            <a class="similaire-card" href="{{ url('annonce/'.strval($similaire->idannonce)) }}">
+                <div class="similaire-card-img">
+                    @if(isset($similaire->photo) && count($similaire->photo) > 0)
+                        <img class="similaire-image" loading="lazy" src="{{ $similaire->photo[0]->nomphoto }}" alt="Photo annonce"/>
                     @else
                         <div style="width:100%; height:100%; display:flex; align-items:center; justify-content:center; color: var(--text-muted);">
                             Sans photo
                         </div>
                     @endif
                 </div>
-
-                <div class="annonce-info">
-                    <div class="annonce-header">
-                        <h2 class="card-title">{{ $annonce->titre_annonce }}</h2>
-                        <span class="card-price">{{ ceil($annonce->prix_nuit) }}€ / nuit</span>
-                    </div>
-                    
-                    <div class="card-meta">
-                        {{ $annonce->type_hebergement->nom_type_hebergement ?? 'Type inconnu' }} &bull; 
-                        {{ $annonce->nb_personnes_max }} pers &bull; 
-                        {{ $annonce->nb_bebe_max }} bébé
-                    </div>
-
-                    <div class="card-footer">
-                        <span class="location-badge">
-                            <svg class="icon-pin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" width="16" height="16">
-                                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                                <circle cx="12" cy="10" r="3"></circle>
-                            </svg>
-                            {{ $annonce->adresse_annonce }} &bull; {{ $annonce->ville->nomville ?? 'Ville' }}
-                        </span>
+                <div class="similaire-info">
+                        <h2 class="similaire-card-title">{{ $similaire->titre_annonce }}</h2>
+                        <span class="similaire-card-price">{{ ceil($similaire->prix_nuit) }}€ / nuit</span>
                         
-                        <span class="date-badge">
-                            {{ \Carbon\Carbon::parse($annonce->date_publication)->format('d M Y') }}
-                        </span>
-                    </div>
+                        <div class="similaire-card-meta">
+                            {{ $similaire->type_hebergement->nom_type_hebergement ?? 'Type inconnu' }} &bull; 
+                            {{ $similaire->nb_personnes_max }} pers &bull; 
+                            {{ $similaire->nb_bebe_max }} bébé
+                        </div>
+
+                        <div class="card-footer">
+                            <span class="location-badge">
+                                <svg class="icon-pin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" width="16" height="16">
+                                    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                                    <circle cx="12" cy="10" r="3"></circle>
+                                </svg>
+                                {{ $similaire->adresse_annonce }} &bull; {{ $similaire->ville->nom_ville ?? 'Ville' }}
+                            </span>
+                        </div>
                 </div>
             </a>
-        @endforeach
+            @empty
+                <div class="res-empty">
+                    <p>Aucun favoris.</p>
+                </div>
+            @endforelse
+            </div>
+        </div>
     </div>
     
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,6 @@
 @section('title', 'Bienvenue')
 
 @section('content')
-  <p class="section-title">Annonces proche de vous</p>
-  <!-- TODO: liste des annonces dans la même ville -->
+    <h2>Découvrez les dernières annonces publiées par nos utilisateurs en effetuant une recherche</h2>
 
-  <p class="section-title">Annonces pour les familles</p>
-  <!-- TODO: liste des annonce avec plus de 2 adults et plus de 1 enfant -->
 @endsection
